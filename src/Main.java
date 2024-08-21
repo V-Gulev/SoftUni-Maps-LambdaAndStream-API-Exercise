6176a21e8bd9f19ac7516022e8c729dd5bf3da79
V-Gulev <velizargulev@abv.bg>
2024-08-21 17:14:45 +0300

    Count Real Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] nums = Arrays.stream(scanner.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+        TreeMap<Double, Integer> count =new TreeMap<>();
+
+        for (double num : nums) {
+            if (!count.containsKey(num)) {
+                count.put(num, 0);
+            }
+            count.put(num, count.get(num) + 1);
+        }
+
+        for (Map.Entry<Double, Integer> entry : count.entrySet()) {
+            DecimalFormat df = new DecimalFormat("#.#######");
+            System.out.printf("%s -> %d%n" ,df.format(entry.getKey()), entry.getValue());
+        }
+    }
 }
